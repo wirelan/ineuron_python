69da5bf0577bfd7f34e219a59e18179b4086cfd6
wirelan <103507928+wirelan@users.noreply.github.com>
2022-11-20 13:24:10 +0530

    Create python_input_output.py

---
+++ python_input_output.py
@@ -0,0 +1,16 @@
+# Declare and assign variables
+int_var = 10   # python will understand its a variable type integer
+float_var = 18.5 # python will understand its a variable type float
+string_var = 'ineuron batch2' # We can also write strin in double quote "ineuron batch2"
+bool_var = True  # If we need to input False then we can mention False
+
+
+#Function in python for output
+
+print("Hello World")
+print("The value of int_var =", int_var)
+print("The value of float_var =",float_var)
+print("The value of string_var =",string_var)
+print("The value of bool_var =",bool_var)
+
+
